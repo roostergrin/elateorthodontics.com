4fbeb21dcc671c84b3318e347e9d9b42a1a6d0c6
luketevers <luketevers@gmail.com>
2019-03-04 13:23:46 -0800

    feat(scroll-top): creates, not used yet;

--- src/resources/components.js
+++ src/resources/components.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Icon from 'components/global/icon/icon'
 import LazyImage from 'components/global/lazy-image/lazy-image'
 import Loader from 'components/global/loader/loader'
+import ScrollTop from 'components/global/scroll-top/scroll-top'
 import NavTrigger from 'components/global/nav-trigger/nav-trigger'
 import Trigger from 'components/global/trigger/trigger'
 
@@ -9,6 +10,7 @@ const components = () => {
   Vue.component('Icon', Icon)
   Vue.component('LazyImage', LazyImage)
   Vue.component('Loader', Loader)
+  Vue.component('ScrollTop', ScrollTop)
   Vue.component('NavTrigger', NavTrigger)
   Vue.component('Trigger', Trigger)
 }
