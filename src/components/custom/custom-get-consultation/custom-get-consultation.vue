c6e246a42065e4bd2ae5e09f7cc308719b91a72c
luketevers <luketevers@gmail.com>
2019-03-05 10:32:40 -0800

    refactor(custom-get-consultation): smoothes scroll effects; updates(main-form && referral-form): changes label positions;

--- src/components/custom/custom-get-consultation/custom-get-consultation.vue
+++ src/components/custom/custom-get-consultation/custom-get-consultation.vue
@@ -37,23 +37,23 @@ export default {
       let halfContainerHeight = this.$refs.component.clientHeight / 2
 
       // Checks if scrolling out of image
-      let bottomHalf = (window.pageYOffset + window.innerHeight) > (this.$refs.component.offsetTop + this.$refs.component.clientHeight)
+      let bottomHalf = window.pageYOffset >= this.$refs.component.offsetTop
 
       // Checks if scrolling into the image
-      let topHalf = (window.pageYOffset + window.innerHeight) < (this.$refs.component.offsetTop + this.$refs.component.clientHeight)
+      let topHalf = window.pageYOffset <= this.$refs.component.offsetTop
 
       // Finds offset distance from half
       let halfOffset = (this.$refs.component.offsetTop - window.pageYOffset) / 100
       let negHalfOffset = -1 * halfOffset
 
       if (halfContainerHeight >= windowTopOffest && topHalf) {
-        this.percentage = 1 + ((halfOffset + 0.37) / 50)
-        this.transform = -1 * (negHalfOffset - 0.37)
+        this.percentage = 1 + (halfOffset / 50)
+        this.transform = halfOffset * 1.5
       }
 
       if (halfContainerHeight >= windowTopOffest && bottomHalf) {
-        this.percentage = 1 + ((negHalfOffset - 0.39) / 50)
-        this.transform = -1 * (halfOffset + 0.39)
+        this.percentage = 1 + (negHalfOffset / 50)
+        this.transform = negHalfOffset * 1.5
       }
     }
   }
