7e0ba35f5c942832d707a0e20cfd8629c759c732
luke.evers <luke.evers@roostergrin.com>
2019-03-11 18:23:55 -0700

    feat(block-overlap-tablet && block-scroll-tablet): completes on all tablet screens;

---
+++ src/components/block/block-scroll-tablet/block-scroll-tablet.vue
@@ -0,0 +1,35 @@
+<template lang="pug" src="./block-scroll-tablet.pug"></template>
+
+<script>
+export default {
+  props: ['content'],
+  data () {
+    return {
+      intersectionOptions: {
+        root: null,
+        rootMargin: '0px 0px 0px 0px',
+        threshold: [0]
+      },
+      active: 0,
+      showing: false,
+      transformed: false
+    }
+  },
+  methods: {
+    onWaypointIn ({going, direction, _entry}) {
+      if (going === 'in' && direction === 'top') {
+        this.showing = true
+        setTimeout(() => {
+          this.transformed = true
+        }, 1250)
+      }
+    },
+    cleanStr (i) {
+      return i.toLowerCase().replace(/[.,/#!$%^&*;:{}=\-_`~()\s]/g, '')
+    },
+    setActive (i) {
+      this.active = i
+    }
+  }
+}
+</script>
