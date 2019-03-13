97d501ddb48a940134f3c9d6b7b731ce02dac9a3
luketevers <luketevers@gmail.com>
2019-03-13 09:40:31 -0700

    updates(block-overlap-mobile): creates on mobile

--- src/components/block/block-overlap-mobile/block-overlap-mobile.vue
+++ src/components/block/block-overlap-mobile/block-overlap-mobile.vue
@@ -10,13 +10,14 @@ export default {
         rootMargin: '0px 0px 0px 0px',
         threshold: [0]
       },
-      scrolling: null,
-      active: 0,
-      showing: false,
-      transformed: false
+      loaded: false,
+      active: false,
+      transform: null,
+      percentage: null
     }
   },
   mounted () {
+    this.loaded = true
     this.$nextTick(
       window.addEventListener('scroll', this.onScroll)
     )
@@ -25,18 +26,36 @@ export default {
     window.removeEventListener('scroll', this.onScroll)
   },
   methods: {
-    cleanStr (i) {
-      return i.toLowerCase().replace(/[.,/#!$%^&*;:{}=\-_`~()\s]/g, '')
+    onWaypoint ({going, direction, _entry}) {
+      if (going === 'in') {
+        this.active = true
+      }
     },
-    setActive (i) {
-      this.active = i
-    },
-    onWaypointIn ({going, direction, _entry}) {
-      if (going === 'in' && direction === 'top') {
-        this.showing = true
-        setTimeout(() => {
-          this.transformed = true
-        }, 1250)
+    onScroll () {
+      // Checks for location of top of page
+      let windowTopOffest = window.pageYOffset - this.$refs.component.offsetTop
+
+      // Finds height of container
+      let halfContainerHeight = this.$refs.component.clientHeight / 2
+
+      // Checks if scrolling out of image
+      let bottomHalf = window.pageYOffset >= this.$refs.component.offsetTop
+
+      // Checks if scrolling into the image
+      let topHalf = window.pageYOffset <= this.$refs.component.offsetTop
+
+      // Finds offset distance from half
+      let halfOffset = (this.$refs.component.offsetTop - window.pageYOffset) / 100
+      let negHalfOffset = -1 * halfOffset
+
+      if (halfContainerHeight >= windowTopOffest && topHalf) {
+        this.percentage = 1 + (halfOffset / 50)
+        this.transform = negHalfOffset * 1.5
+      }
+
+      if (halfContainerHeight >= windowTopOffest && bottomHalf) {
+        this.percentage = 1 + (negHalfOffset / 50)
+        this.transform = halfOffset * 1.5
       }
     }
   }
