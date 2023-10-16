e96e44e163cd24436e63b03d134abae7b20aefa3
Sebastian Park <me@sebastian-park.com>
2023-10-16 10:15:08 -0700

    requests(index.php) adds gtm script 74926

--- dist/index.php
+++ dist/index.php
@@ -10,10 +10,20 @@
   	  window.dataLayer = window.dataLayer || [];
   	  function gtag(){dataLayer.push(arguments);}
   	  gtag('js', new Date());
-  
+
   	  gtag('config', 'G-84T7FKXW08');
-  	</script>  
+  	</script>
     <?php wp_head(); ?>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    '
+    https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f)
+    ;
+    })(window,document,'script','dataLayer','GTM-PP9TFH5B');</script>
+    <!-- End Google Tag Manager -->
 
 	<!-- Facebook Pixel Code -->
 	<script>
@@ -32,9 +42,16 @@
 	src="https://www.facebook.com/tr?id=2401702583464237&ev=PageView&noscript=1"
 	/></noscript>
 	<!-- End Facebook Pixel Code -->
-    
+
 </head>
 <body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="
+  https://www.googletagmanager.com/ns.html?id=GTM-PP9TFH5B"
+  height="0" width="0"
+  style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
   <div id='app' role='application'></div>
   <?php wp_footer(); ?>
   <script type="text/javascript">
@@ -53,18 +70,18 @@
   <script src="https://onlineschedulingv2.threadcommunication.com" type="text/javascript"></script>
   <script type="text/javascript"> OpenChair.init({ token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmlnaW4iOiJlbGF0ZW9ydGhvZG9udGljcy5jb20iLCJwcmFjdGljZV9pZCI6IjMzNCJ9.9JbW9QH1McnjmtVsO2lV4ePa1kRvG1hSz5DTKmOGoRk", mainButtonLeft: true, }) </script>
 
-  <script type="text/javascript"> 
+  <script type="text/javascript">
   (function(d){
     var s = d.createElement("script");
     s.setAttribute("data-account", "D3656BNpyD");
     s.setAttribute("src", "https://cdn.userway.org/widget.js");
     (d.body || d.head).appendChild(s);})(document);
- 
+
   (function(d){
     var ns = d.createElement("noscript");
     ns.innerHTML = `Please ensure Javascript is enabled for purposes of <a href="https://userway.org">website accessibility</a>`;
-    (d.body || d.head).appendChild(ns);})(document);    
+    (d.body || d.head).appendChild(ns);})(document);
 </script>
-  
+
   </body>
 </html>
